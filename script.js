03f4a0e23d3ed728275fa7e83e5bc3950f230c82
Tomki9 <88100719+Tomki9@users.noreply.github.com>
2023-05-04 21:08:42 -0400

    california

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var count = 0;
-
-function changeCount(num){
-    count+= num;
-
-    document.getElementById('count').innerHTML= count;
-}
+$(document).ready(function(){
+    $(".accordion-titel").click(function(){
+    $(this).parent(".accordion-item").find(".accordion-contant").slideToggle();
+    $(this).parent(".accordion-item").prevAll(".accordion-item").find(".accordion-contant").slideUp();
+    $(this).parent(".accordion-item").nextAll(".accordion-item").find(".accordion-contant").slideUp();
+    });
+});
